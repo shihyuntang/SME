e5745908ff483da9d3ff6b7b80da8d30239c24aa
Ansgar Wehrhahn <31626864+AWehrhahn@users.noreply.github.com>
2020-11-10 13:41:39 +0100

    Fix files on server

---
+++ examples/create_files_for_server.py
@@ -0,0 +1,8 @@
+from pysme.large_file_storage import setup_atmo, setup_nlte
+
+lfs_atmo = setup_atmo()
+lfs_nlte = setup_nlte()
+
+for lfs in [lfs_atmo, lfs_nlte]:
+    pointers = lfs.generate_pointers()
+    pass
--- examples/prepare_files_for_server.py
+++ examples/prepare_files_for_server.py
@@ -4,6 +4,9 @@ lfs_atmo = setup_atmo()
 lfs_nlte = setup_nlte()
 
 for name, lfs in zip(["atmospheres", "nlte"], [lfs_atmo, lfs_nlte]):
+    for key in lfs.pointers.keys():
+        print(key)
+        lfs.get(key)
     pointers = lfs.generate_pointers()
     lfs.create_pointer_file(f"datafiles_{name}.json")
     lfs.move_to_cache()
