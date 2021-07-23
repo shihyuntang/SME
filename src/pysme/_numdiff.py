Fixes the libary, but parallel gives wrong results

src/pysme/_numdiff.py
@@ -1,3 +1,4 @@
+from concurrent import futures
 import numpy as np
 from scipy.optimize._numdiff import (
     _prepare_bounds,
@@ -11,6 +12,7 @@ from scipy.optimize._numdiff import (
     _sparse_difference,
 )
 from pathos.multiprocessing import ProcessPool
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 
 def approx_derivative(
@@ -331,10 +333,21 @@ def _dense_difference(fun, x0, f0, h, use_one_sided, method):
     func = _dense_func(method, fun, x0, f0, h_vecs, use_one_sided)
 
     iparam = np.arange(h.size)
-    # data = [func(i) for i in iparam]
 
-    with ProcessPool() as pool:
-        data = pool.map(func, iparam)
+    # Sequential version for debugging
+    data = [func(i) for i in iparam]
+
+    # Use Pathos ProcessPool so we can pickle the local function sme.synthesize_spectrum
+    # with ProcessPool() as pool:
+    #     data = pool.map(func, iparam)
+
+    # For comparison this is what the default would be if it worked
+    # data = [None for _ in iparam]
+    # with ProcessPoolExecutor() as executor:
+    #     futures = {executor.submit(func, i): i for i in iparam}
+    #     for future in as_completed(futures):
+    #         i = futures[future]
+    #         data[i] = future.result()
 
     for i in iparam:
         dx, df = data[i]

src/pysme/continuum_and_radial_velocity.py
@@ -39,13 +39,17 @@ def apply_radial_velocity(wave, wmod, smod, vrad, segments):
     return smod
 
 
-def apply_continuum(wave, smod, cscale, cscale_type, segments):
+def apply_continuum(wave, smod, cwave, cscale, cscale_type, segments):
     if cscale is None:
         return smod
     for il in segments:
         if cscale[il] is not None and not np.all(cscale[il] == 0):
             if cscale_type in ["spline"]:
-                smod[il] *= cscale[il]
+                if len(cscale[il]) != len(smod[il]):
+                    cs = np.interp(wave[il], cwave[il], cscale[il])
+                else:
+                    cs = cscale[il]
+                smod[il] *= cs
             else:
                 x = wave[il] - wave[il][0]
                 smod[il] *= np.polyval(cscale[il], x)
@@ -82,7 +86,8 @@ def apply_radial_velocity_and_continuum(
         the corrected synthetic spectrum
     """
     smod = apply_radial_velocity(wave, wmod, smod, vrad, segments)
-    smod = apply_continuum(wave, smod, cscale, cscale_type, segments)
+    # The radial velocity shift also interpolates onto the wavelength grid
+    smod = apply_continuum(wave, smod, wave, cscale, cscale_type, segments)
     return smod
 
 
@@ -295,12 +300,17 @@ def determine_radial_velocity(
         if sme.vrad_flag == "each":
             # apply continuum
             y_syn = apply_continuum(
-                {segment: x_syn}, {segment: y_syn}, cscale, sme.cscale_type, [segment]
+                {segment: x_syn},
+                {segment: y_syn},
+                sme.wave,
+                cscale,
+                sme.cscale_type,
+                [segment],
             )[segment]
         elif sme.vrad_flag == "whole":
             # All segments
             y_syn = apply_continuum(
-                x_syn, y_syn, cscale, sme.cscale_type, range(len(y_obs))
+                x_syn, y_syn, sme.wave, cscale, sme.cscale_type, range(len(y_obs))
             )
 
             x_obs = x_obs.ravel()
@@ -752,7 +762,7 @@ def cont_fit(sme, segment, x_syn, y_syn, rvel=0, only_mask=False):
 
 def get_continuum_broadening(sme, segment, x_syn, y_syn, rvel=0, only_mask=False):
     if sme.cscale_flag in ["none"]:
-        return [1]
+        return np.ones(len(sme.spec[segment]))
     elif sme.cscale_flag in ["fix"]:
         return sme.cscale[segment]
 
@@ -881,21 +891,27 @@ def match_rv_continuum(sme, segments, x_syn, y_syn):
                 sme, s, cscale[s], [x_syn[s] for s in s], [y_syn[s] for s in s]
             )
     elif sme.cscale_type in ["spline"]:
-        for s in segments:
-            # We only use the continuum mask for the continuum fit,
-            # we need the lines for the radial velocity
-            vrad[s] = determine_radial_velocity(
-                sme, s, cscale[s], x_syn[s], y_syn[s], only_mask=False
-            )
-            cscale[s] = get_continuum_broadening(
-                sme, s, x_syn[s], y_syn[s], rvel=vrad[s], only_mask=False
-            )
-
-        if sme.vrad_flag == "whole":
+        if sme.vrad_flag == "each":
+            for s in segments:
+                # We only use the continuum mask for the continuum fit,
+                # we need the lines for the radial velocity
+                vrad[s] = determine_radial_velocity(
+                    sme, s, cscale[s], x_syn[s], y_syn[s], only_mask=False
+                )
+                cscale[s] = get_continuum_broadening(
+                    sme, s, x_syn[s], y_syn[s], rvel=vrad[s], only_mask=False
+                )
+        elif sme.vrad_flag == "whole":
             s = segments
             vrad[s] = determine_radial_velocity(
                 sme, s, cscale[s], [x_syn[s] for s in s], [y_syn[s] for s in s]
             )
+            for s in segments:
+                cscale[s] = get_continuum_broadening(
+                    sme, s, x_syn[s], y_syn[s], rvel=vrad[s], only_mask=False
+                )
+        else:
+            raise ValueError
     else:
         raise ValueError(
             f"Did not understand cscale_type, expected one of ('whole', 'mask'), but got {sme.cscale_type}."

src/pysme/sme_synth.py
@@ -1,11 +1,12 @@
 """ Wrapper for sme_synth.so C library """
 import os
-from os.path import dirname, join
 import logging
+from pathlib import Path
 
 import numpy as np
 
-from .cwrapper import IDL_DLL
+from .cwrapper import get_lib_name, IDL_DLL
+from .libtools import get_full_datadir
 
 logger = logging.getLogger(__name__)
 
@@ -41,11 +42,16 @@ class SME_DLL:
         self.ion = None
 
         self.lib = IDL_DLL(libfile)
-        self.parallel = False
-        if state is None:
-            self.state = self.NewState()
+
+        if self.interface == "Parallel":
+            self.parallel = True
+            if state is None:
+                self.state = self.NewState()
+            else:
+                self.state = state
         else:
-            self.state = state
+            self.parallel = False
+            self.state = None
 
         self.SetLibraryPath(datadir)
         self.check_data_files_exist()
@@ -58,6 +64,10 @@ class SME_DLL:
     def libfile(self):
         """str: Location of the library file"""
         return self.lib.libfile
+
+    @property
+    def interface(self):
+        return self.lib.interface
 
     @property
     def file(self):
@@ -73,15 +83,20 @@ class SME_DLL:
     @property
     def ndepth(self):
         """int: Number of depth layers in the atmosphere model"""
-        assert self.atmo is not None, f"No model atmosphere has been set"
+        assert self.atmo is not None, "No model atmosphere has been set"
         motype = self.atmo.depth
         return len(self.atmo[motype])
 
     @property
     def nlines(self):
         """int: number of lines in the linelist"""
-        assert self.linelist is not None, f"No line list has been set"
+        assert self.linelist is not None, "No line list has been set"
         return len(self.linelist)
+
+    @property
+    def file(self):
+        """str: (Expected) Location of the library file"""
+        return get_lib_name()
 
     def check_data_files_exist(self):
         """
@@ -99,18 +114,15 @@ class SME_DLL:
             n = os.path.join(directory, name)
             if not os.path.exists(n):
                 raise FileNotFoundError(
-                    f"Could not find required data file {name} in library directory {directory}"
+                    "Could not find required data file {name} in library directory {directory}".format(
+                        name=name, directory=directory
+                    )
                 )
 
     def NewState(self, delete_old=True):
         if delete_old and hasattr(self, "state") and self.state is not None:
             self.FreeState()
         self.state = self.lib.new_state()
-        if self.state is None:
-            self.parallel = False
-        else:
-            self.parallel = True
-
         return self.state
 
     def FreeState(self):
@@ -161,16 +173,6 @@ class SME_DLL:
         )
         return version
 
-    def SetLibraryPath(self, datadir=None):
-        """ Set the path to the library """
-        if datadir is None:
-            datadir = join(dirname(__file__), "share", "libsme")
-        datadir = str(datadir)
-        # Against all conventions, SME lib expects a "/" at the end of the path
-        if not datadir.endswith(os.sep):
-            datadir = datadir + os.sep
-        self.lib.SetLibraryPath(datadir, type="string", state=self.state)
-
     def GetLibraryPath(self):
         """ Get the data file directory """
         return self.lib.GetLibraryPath(raise_error=False, state=self.state)
@@ -178,13 +180,13 @@ class SME_DLL:
     def GetDataFiles(self):
         """ Get the required data files """
         files = self.lib.GetDataFiles(raise_error=False, state=self.state)
-        if files != "Using obsolete SME Library":
-            return files.split(";")
-        else:
-            logging.warning(
-                "Using obsolete SME Library. Cannot determine necessay data files"
-            )
-            return []
+        return files.split(";")
+
+    def SetLibraryPath(self, libpath=None):
+        """ Set the path to the library """
+        if libpath is None:
+            libpath = get_full_datadir()
+        self.lib.SetLibraryPath(libpath, type="string", state=self.state)
 
     def InputWaveRange(self, wfirst, wlast):
         """
@@ -216,14 +218,11 @@ class SME_DLL:
         gamma6 : float
             van der Waals scaling factor
         """
-        logger.debug("Setting Van der Waals scale in smelib")
         self.lib.SetVWscale(gamma6, type="double", state=self.state)
         self.vw_scale = gamma6
 
     def SetH2broad(self, h2_flag=True):
         """ Set flag for H2 molecule """
-        logger.debug("Setting H2 broadening in smelib")
-
         if h2_flag:
             self.lib.SetH2broad(state=self.state)
             self.h2broad = True
@@ -248,22 +247,29 @@ class SME_DLL:
             names of the elements (with Ionization level)
         """
         try:
-            atomic = linelist.atomic.T
-            species = linelist.species
+            atomic = linelist["atomic"].T
+            species = linelist["species"]
         except AttributeError:
             raise TypeError("linelist has to be a LineList type")
 
-        nlines = len(linelist)
+        nlines = len(species)
         species = np.asarray(species, "U8")
 
         assert (
             atomic.shape[1] == nlines
-        ), f"Got wrong Linelist shape, expected ({nlines}, 8) but got {atomic.shape}"
+        ), "Got wrong Linelist shape, expected ({nlines}, 8) but got {atomic}".format(
+            nlines=nlines, atomic=atomic.shape
+        )
         assert (
             atomic.shape[0] == 8
-        ), f"Got wrong Linelist shape, expected ({nlines}, 8) but got {atomic.shape}"
+        ), "Got wrong Linelist shape, expected ({nlines}, 8) but got {atomic}".format(
+            nlines=nlines, atomic=atomic.shape
+        )
 
-        logger.debug("Passing linelist to smelib")
+        if self.state is not None:
+            self.state.contents.free_linelist()
+            self.state.contents.free_opacities()
+
         self.lib.InputLineList(
             nlines, species, atomic, type=("int", "string", "double"), state=self.state
         )
@@ -304,7 +310,9 @@ class SME_DLL:
         nlines = atomic.shape[0]
         assert (
             atomic.shape[1] == 8
-        ), f"Got wrong Linelist shape, expected ({nlines}, 8) but got {atomic.shape}"
+        ), "Got wrong Linelist shape, expected ({nlines}, 8) but got {atomic}".format(
+            nlines=nlines, atomic=atomic.shape
+        )
 
         assert (
             len(index) == nlines
@@ -315,7 +323,6 @@ class SME_DLL:
 
         atomic = atomic.T
 
-        logger.debug("Updating linelist in smelib")
         self.lib.UpdateLineList(
             nlines,
             species,
@@ -346,29 +353,16 @@ class SME_DLL:
             raise ValueError("Turbulence velocity must be positive or zero")
 
         try:
-
-            #   MOTYPE==0   means depth scale is "Tau", plane-parralel
-            #   MOTYPE==1   means depth scale is "Rhox", plane-parralel
-            #   MOTYPE==3   means depth scale is "RhoX", spherical
-            #   MOTYPE==-1  fake value used with the call to OPMTRX get just
-            #               just the line opacities
-
-            if atmo.geom == "SPH":
-                # Spherical only supports RHOX
-                depth = atmo["RHOX"]
-                motype = "SPH"
-            else:
-                motype = atmo.depth
-                depth = atmo[motype]
-
+            motype = atmo["depth"]
+            depth = atmo[motype]
             ndepth = len(depth)
-            t = atmo.temp
-            xne = atmo.xne
-            xna = atmo.xna
-            rho = atmo.rho
+            t = atmo["temp"]
+            xne = atmo["xne"]
+            xna = atmo["xna"]
+            rho = atmo["rho"]
             vt = np.full(ndepth, vturb) if np.size(vturb) == 1 else vturb
-            wlstd = atmo.wlstd
-            opflag = atmo.opflag
+            wlstd = atmo["wlstd"]
+            opflag = atmo["opflag"]
             args = [
                 ndepth,
                 teff,
@@ -385,16 +379,18 @@ class SME_DLL:
             ]
             type = "sdddusdddddd"  # s : short, d: double, u: unicode (string)
 
-            if atmo.geom == "SPH":
-                radius = atmo.radius
-                height = atmo.height
+            if atmo["geom"] == "SPH":
+                radius = atmo["radius"]
+                height = atmo["height"]
                 motype = "SPH"
-                args = args[:4] + [motype, radius] + args[5:] + [height]
-                type = type[:4] + "ud" + type[5:] + "d"
+                args = args[:5] + [radius] + args[5:] + [height]
+                type = type[:5] + "d" + type[5:] + "d"
         except AttributeError as ae:
-            raise TypeError(f"atmo has to be an Atmo type, {ae}")
+            raise TypeError("atmo has to be an Atmo type, {ae}".format(ae=ae))
 
-        logger.debug("Inputing atmosphere model to smelib")
+        if self.state is not None:
+            self.state.contents.free_opacities()
+
         self.lib.InputModel(*args, type=type, state=self.state)
 
         self.teff = teff
@@ -420,9 +416,6 @@ class SME_DLL:
         # metallicity is included in the abundance class, ignored in function call
         abund = abund("sme", raw=True)
         assert isinstance(abund, np.ndarray)
-        abund[np.isnan(abund)] = -99
-
-        logger.debug("Inputing abundances to smelib")
         self.lib.InputAbund(abund, type="double", state=self.state)
 
         self.abund = abund
@@ -440,27 +433,24 @@ class SME_DLL:
 
         Returns
         -------
-        copblu : array of size (nrhox,)
+        copblu : array of size (nmu,)
             only if getData is True
-        copred : array of size (nrhox,)
+        copred : array of size (nmu,)
             only if getData is True
-        copstd : array of size (nrhox,)
+        copstd : array of size (nmu,)
             only if getData is True and motype is 0
         """
         args = []
         type = ""
         if getData:
-            assert (
-                self.atmo is not None
-            ), "Need to set the atmosphere model before retrievieng Opacities"
-            nrhox = len(self.atmo.rhox)
-            copblu = np.zeros(nrhox)
-            copred = np.zeros(nrhox)
-            args = [nrhox, copblu, copred]
+            nmu = self.nmu
+            copblu = np.zeros(nmu)
+            copred = np.zeros(nmu)
+            args = [nmu, copblu, copred]
             type = ["s", "d", "d"]
 
             if motype == 0:
-                copstd = np.zeros(nrhox)
+                copstd = np.zeros(nmu)
                 args += [copstd]
                 type += ["d"]
 
@@ -535,7 +525,9 @@ class SME_DLL:
         ion : int
             flag that determines the behaviour of the C function
         """
-        logger.debug("Calculating ionization in smelib")
+        if self.state is not None:
+            self.state.contents.free_ionization()
+
         self.lib.Ionization(
             ion, type="short", raise_error=False, raise_warning=True, state=self.state
         )
@@ -646,11 +638,9 @@ class SME_DLL:
         sint_seg = np.zeros((nwmax, nmu))  # line+continuum intensities
         cint_seg = np.zeros((nwmax, nmu))  # all continuum intensities
         cintr_seg = np.zeros((nmu))  # red continuum intensity
-        nw = np.array([nw])
 
         type = "sdddiiddddss"  # s: short, d:double, i:int, u:unicode (string)
 
-        logger.debug("Starting radiative Transfer calculations in smelib")
         self.lib.Transf(
             nmu,
             mu,
@@ -667,8 +657,9 @@ class SME_DLL:
             type=type,
             state=self.state,
         )
-        # nw = nw[0]
-        nw = np.count_nonzero(wint_seg)
+
+        if nw == 0:
+            nw = np.count_nonzero(wint_seg)
 
         wint_seg = wint_seg[:nw]
         sint_seg = sint_seg[:nw, :].T
@@ -733,18 +724,15 @@ class SME_DLL:
         csf : array
             Continuum source function
         """
-        assert (
-            self.atmo is not None
-        ), "Need to set the atmosphere model before retrievieng Opacities"
-        nrhox = len(self.atmo.rhox)
-        lop = np.zeros(nrhox)
-        cop = np.zeros(nrhox)
-        scr = np.zeros(nrhox)
-        tsf = np.zeros(nrhox)
-        csf = np.zeros(nrhox)
+        nmu = self.ndepth
+        lop = np.zeros(nmu)
+        cop = np.zeros(nmu)
+        scr = np.zeros(nmu)
+        tsf = np.zeros(nmu)
+        csf = np.zeros(nmu)
         type = "dsddddd"
         self.lib.GetLineOpacity(
-            wave, nrhox, lop, cop, scr, tsf, csf, type=type, state=self.state
+            wave, nmu, lop, cop, scr, tsf, csf, type=type, state=self.state
         )
         return lop, cop, scr, tsf, csf
 
@@ -789,13 +777,17 @@ class SME_DLL:
             raise TypeError("Departure coefficient matrix is not an array")
 
         bmat = np.atleast_2d(bmat)
-        if bmat.shape[1] != 2:
+        if bmat.shape[0] != 2:
             raise ValueError(
-                f"Departure coefficient matrix has the wrong shape, expected ({ndepth}, 2) but got {bmat.shape} instead"
+                "Departure coefficient matrix has the wrong shape, expected (2, {ndepth}) but got {bmat} instead".format(
+                    ndepth=ndepth, bmat=bmat.shape
+                )
             )
-        if bmat.shape[0] != ndepth:
+        if bmat.shape[1] != ndepth:
             raise ValueError(
-                f"Departure coefficient matrix has the wrong shape, expected ({ndepth}, 2) but got {bmat.shape} instead"
+                "Departure coefficient matrix has the wrong shape, expected (2, {ndepth}) but got {bmat} instead".format(
+                    ndepth=ndepth, bmat=bmat.shape
+                )
             )
 
         if not isinstance(lineindex, (int, np.integer)):
@@ -803,7 +795,9 @@ class SME_DLL:
 
         if not 0 <= lineindex < nlines:
             raise ValueError(
-                f"Lineindex out of range, expected value between 0 and {nlines}, but got {lineindex} instead"
+                "Lineindex out of range, expected value between 0 and {nlines}, but got {lineindex} instead".format(
+                    nlines=nlines, lineindex=lineindex
+                )
             )
 
         self.lib.InputDepartureCoefficients(

src/pysme/solve.py
@@ -49,8 +49,8 @@ class SME_Solver:
         self.restore = restore
 
         # For displaying the progressbars
-        self.progressbar = None
-        self.progressbar_jacobian = None
+        # self.progressbar = None
+        # self.progressbar_jacobian = None
 
     @property
     def nparam(self):
@@ -127,7 +127,8 @@ class SME_Solver:
         save = not isJacobian and self.filename is not None
         reuse_wavelength_grid = isJacobian
         radial_velocity_mode = "robust" if not isJacobian else "fast"
-        method = "parallel" if isJacobian else "sequential"
+        # method = "parallel" if isJacobian else "sequential"
+        method = "sequential"
 
         # change parameters
         for name, value in zip(self.parameter_names, param):
@@ -139,7 +140,7 @@ class SME_Solver:
                 updateStructure=update,
                 reuse_wavelength_grid=reuse_wavelength_grid,
                 segments=segments,
-                passLineList=False,
+                passLineList=method == "parallel",
                 updateLineList=self.update_linelist,
                 radial_velocity_mode=radial_velocity_mode,
                 method=method,
@@ -170,11 +171,11 @@ class SME_Solver:
         resid = np.nan_to_num(resid, copy=False)
 
         # Update progress bars
-        if isJacobian:
-            self.progressbar_jacobian.update(1)
-        else:
-            self.progressbar.total += 1
-            self.progressbar.update(1)
+        # if isJacobian:
+        #     self.progressbar_jacobian.update(1)
+        # else:
+        #     self.progressbar.total += 1
+        #     self.progressbar.update(1)
 
         if not isJacobian:
             # Save result for jacobian
@@ -195,7 +196,10 @@ class SME_Solver:
         The calculation is the same as "3-point"
         but we can tell residuals that we are within a jacobian
         """
-        self.progressbar_jacobian.reset()
+        # self.progressbar_jacobian.reset()
+
+        # Here we replace the scipy version of approx_derivative with our own
+        # The only difference being that we use Multiprocessing for the jacobian
         g = approx_derivative(
             self.__residuals,
             param,
@@ -655,8 +659,8 @@ class SME_Solver:
 
         # Do the heavy lifting
         if self.nparam > 0:
-            self.progressbar = tqdm(desc="Iteration", total=0)
-            self.progressbar_jacobian = tqdm(desc="Jacobian", total=len(p0))
+            # self.progressbar = tqdm(desc="Iteration", total=0)
+            # self.progressbar_jacobian = tqdm(desc="Jacobian", total=len(p0))
             with print_to_log():
                 res = least_squares(
                     self.__residuals,
@@ -671,8 +675,8 @@ class SME_Solver:
                     args=(sme, spec, uncs, mask),
                     kwargs={"bounds": bounds, "segments": segments},
                 )
-            self.progressbar.close()
-            self.progressbar_jacobian.close()
+            # self.progressbar.close()
+            # self.progressbar_jacobian.close()
             # The returned jacobian is "scaled for robust loss function"
             res.jac = self._last_jac
             for i, name in enumerate(self.parameter_names):

src/pysme/synthesize.py
@@ -1,6 +1,7 @@
 """
 Spectral Synthesis Module of SME
 """
+from concurrent import futures
 import logging
 from os import stat
 import warnings
@@ -16,6 +17,8 @@ from scipy.interpolate import interp1d, UnivariateSpline
 import uuid
 from pathos.multiprocessing import ProcessPool
 from pathos.pools import ThreadPool
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from . import broadening
 from .atmosphere.interpolation import AtmosphereInterpolator
@@ -440,7 +443,7 @@ class Synthesizer:
     def sequential_synthesize_segments(
         self, sme, segments, wmod, smod, cmod, reuse_wavelength_grid, dll_id=None
     ):
-        for il in tqdm(segments, desc="Segment", leave=False):
+        for il in segments:
             wmod[il], smod[il], cmod[il] = self.synthesize_segment(
                 sme, il, reuse_wavelength_grid, il != segments[0], dll_id=dll_id
             )
@@ -490,13 +493,28 @@ class Synthesizer:
 
         def parallel(il):
             return self.synthesize_segment(
-                sme, il, reuse_wavelength_grid, True, method="parallel", dll_id=dll_id,
+                sme,
+                il,
+                reuse_wavelength_grid,
+                True,
+                method="sequential",
+                dll_id=dll_id,
             )
 
+        # Sequential version for debugging
         data = [parallel(il) for il in segments[1:]]
 
+        # Pathos version crashes for some reason
         # with ThreadPool() as pool:
         #     data = pool.map(parallel, segments[1:])
+
+        # Use "default" ThreadPool instead
+        # data = [None for _ in segments[1:]]
+        # with ThreadPoolExecutor() as executor:
+        #     futures = {executor.submit(parallel, il): il for il in segments[1:]}
+        #     for future in as_completed(futures):
+        #         il = futures[future] - 1
+        #         data[il] = future.result()
 
         for i, seg in enumerate(segments[1:]):
             wmod[seg] = data[i][0]
@@ -587,9 +605,15 @@ class Synthesizer:
             wave = [w for w in sme.wave]
 
         if method == "parallel" and not self.get_dll(dll_id).parallel:
-            logger.warning(
-                "Parallel mode was requested, but the library in use is a sequential version. Running in sequential mode instead"
-            )
+            # display only once
+            if (
+                not hasattr(self, "_warning_parallel_mode")
+                or not self._warning_parallel_mode
+            ):
+                self._warning_parallel_mode = True
+                logger.warning(
+                    "Parallel mode was requested, but the library in use is a sequential version. Running in sequential mode instead"
+                )
             method = "sequential"
 
         if method == "parallel":
@@ -738,6 +762,7 @@ class Synthesizer:
         logger.debug("Segment %i out of %i", segment, sme.nseg)
         if method == "parallel":
             dll = self.get_dll(dll_id).copy()
+            dll_id = self.get_dll_id(dll)
         else:
             dll = self.get_dll(dll_id)
 