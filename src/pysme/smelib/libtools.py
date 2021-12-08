2c16640e401ee10ce1866de1f126d393a0934d60
Ansgar Wehrhahn <31626864+AWehrhahn@users.noreply.github.com>
2021-12-08 11:08:55 +0100

    organize library finding

--- src/pysme/smelib/libtools.py
+++ src/pysme/smelib/libtools.py
@@ -65,6 +65,10 @@ def compile_interface():
 def get_lib_name():
     """Get the name of the sme C library"""
     system = platform.system().lower()
+
+    if system == "windows":
+        return "libsme-5.dll"
+
     arch = platform.machine()
     bits = 64  # platform.architecture()[0][:-3]
 
@@ -73,16 +77,20 @@ def get_lib_name():
     )
 
 
+def get_lib_directory():
+    if platform.system() in ["Windows"]:
+        dirpath = "bin"
+    else:
+        # For Linux/MacOS
+        dirpath = "lib"
+    return dirpath
+
+
 def get_full_libfile():
     """Get the full path to the sme C library"""
     localdir = dirname(dirname(__file__))
     libfile = get_lib_name()
-    # TODO: Or "bin" for Windows
-    if platform.system() in ["Windows"]:
-        dirpath = "bin"
-        libfile = "libsme-5.dll"
-    else:
-        dirpath = "lib"
+    dirpath = get_lib_directory()
     libfile = join(localdir, dirpath, libfile)
     return libfile
 
